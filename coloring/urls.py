7d9ca0c548d798972b508c1a3b909d3a52e50088
Nathaniel Weinman <nathaniel_weinman@berkeley.edu>
2021-07-05 12:21:49 -0700

    Update "demo", create blank copy for students to work with.

--- coloring/urls.py
+++ coloring/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('demo', views.index, name='index'),
+    path('new_interaction', views.index, name='new_interaction')
 ]
